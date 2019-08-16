3264ffd83d8ce66b1de3c15a09062245350c9793
Lauren <Lauren.Musante@gmail.com>
2019-08-16 12:32:07 -0700

    Fixes hide/show results functionality. Still not totally fixed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(funciton(){
+$(document).ready(function(){
   $("form#quiz").submit(function(event){
     event.preventDefault();
     var quantity = $("input:radio[name=quantity]:checked").val();
@@ -6,25 +6,26 @@ $(document).ready(funciton(){
     var consistency = $("input:radio[consistency]:checked").val();
     var omelette = $("input:radio[omelette]:checked").val();
     var bird = $("input:radio[bird]:checked").val();
-    if (!quantity || !mehod || !consistency || !omelette || !bird){
-      (alert("Please enter a response for each question before submitting");
-    }
-    else {
-      ("#result").text();}
-      if (quantity === 1 || method === overmedium || method === scrambled || consistency === hard || (omelette === denver && bird === chicken) || bird === pheasant){
+    // if (!quantity || !method || !consistency || !omelette || !bird){
+    //   (alert("Please enter a response for each question before submitting")
+    // }
+    // else{
+      if (quantity === "1" ||  method === "scrambled" || consistency === "hard" || (omelette === "denver" && bird === "chicken") || bird === "pheasant"){
         $("option1").show();
       }
-      else if (quantity === 2 || method === sunnyside || consistency === softer ||| bird === ostrich || omelette === cheese){
+      else if (quantity === "2" || method === "sunnyside" || consistency === "softer" || bird === "ostrich" || omelette === "cheese"){
         $("option2").show();
       }
-      else if (quantity === 3 || method === poached ||| (method === boiled && consistency === soft) || omelette === florentine || bird === duck){
+      else if (quantity === "3" || method === "poached" || (method === "boiled" && consistency === "soft") || omelette === "florentine" || bird === "duck"){
         $("option3").show();
       }
-      else if (quantity === 6 || method === scrambled || consistency === raw || omelette === meat || bird === ostrich){
+      else if (quantity === "6" || method === "scrambled" || consistency === "raw" || omelette === "meat" || bird === "ostrich"){
         $("option4").show();
       }
       else{
         $("option5").show();
       }
-  })
-}
+      ("#quiz").hide();
+      ("#result").show();
+   });
+ });
