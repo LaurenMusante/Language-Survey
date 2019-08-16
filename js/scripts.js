8495de5e4c12b3cd6f60abf7ceb6457cc0776afc
Lauren <Lauren.Musante@gmail.com>
2019-08-16 11:08:44 -0700

    Add if/else statements to custom js file.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(funciton(){
+  $("form#quiz").submit(function(event){
+    event.preventDefault();
+    var quantity = $("input:radio[name=quantity]:checked").val();
+    var method = $("input:radio[name=method]:checked").val();
+    var consistency = $("input:radio[consistency]:checked").val();
+    var omelette = $("input:radio[omelette]:checked").val();
+    var bird = $("input:radio[bird]:checked").val();
+    if (!quantity || !mehod || !consistency || !omelette || !bird){
+      (alert("Please enter a response for each question before submitting");
+    }
+    else {
+      ("#result").text();}
+      if (quantity === 1 || method === overmedium || method === scrambled || consistency === hard || (omelette === denver && bird === chicken) || bird === pheasant){
+        $("option1").show();
+      }
+      else if (quantity === 2 || method === sunnyside || consistency === softer ||| bird === ostrich || omelette === cheese){
+        $("option2").show();
+      }
+      else if (quantity === 3 || method === poached ||| (method === boiled && consistency === soft) || omelette === florentine || bird === duck){
+        $("option3").show();
+      }
+      else if (quantity === 6 || method === scrambled || consistency === raw || omelette === meat || bird === ostrich){
+        $("option4").show();
+      }
+      else{
+        $("option5").show();
+      }
+  })
+}
