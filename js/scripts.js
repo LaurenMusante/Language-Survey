ad5eda3e4066416648dda4af7189617d7aa85fdb
Lauren <Lauren.Musante@gmail.com>
2019-08-17 12:14:06 -0700

    Add values to inputs so that possible resuts actually show properly.

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,27 @@ $(document).ready(function(){
     event.preventDefault();
     var quantity = $("input:radio[name=quantity]:checked").val();
     var method = $("input:radio[name=method]:checked").val();
-    var consistency = $("input:radio[consistency]:checked").val();
-    var omelette = $("input:radio[omelette]:checked").val();
-    var bird = $("input:radio[bird]:checked").val();
+    var consistency = $("input:radio[name=consistency]:checked").val();
+    var omelette = $("input:radio[name=omelette]:checked").val();
+    var bird = $("input:radio[name=bird]:checked").val();
 
-      if (quantity === "1" || omelette === "denver"){
+      if (quantity === "1" || omelette === "denver" || method === "poached" || bird === "pheasant"){
         $("#option5").show();
       }
-      else if (quantity === "2" || omelette === "meat" || consistency === "hard"){
+      else if (quantity === "2" || omelette === "meat" || omelette === "tamagoyaki" || method === "boiled" || consistency === "hardBoiled"){
         $("#option2").show();
       }
-      else if (quantity === "3" || omelette === "florentine" || bird === "chicken"){
+      else if (quantity === "3" || omelette === "florentine" || method === "sunnySideUp" || bird === "chicken" || consistency === "softBoiled" || omelette === "cheese"){
         $("#option3").show();
       }
-      else if (quantity === "6" || bird === "ostrich" || consistency === "softer" || omelette === "meat"){
-        $("#option3").show();
+      else if (quantity === "6" || bird === "ostrich" || method === "scrambled" || consistency === "softer" || bird === "duck" || omelette === "meat"){
+        $("#option1").show();
       }
-      else if (quantity === "A dozen" || bird === "quail"){
+      else if (quantity === "12" || bird === "quail" || consistency === "raw" || method === "overMedium" || omelette === "whites"){
         $("option4").show();
       }
       else {
-        $("#option1").show();
+        alert("error")
       }
       $("#result").show();
       $("#quiz").hide();
